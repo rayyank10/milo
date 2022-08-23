920ba92aea1e0c7b241bb869bc504b31733084d0
ChrisChrisChris <chrischrischris@users.noreply.github.com>
2022-08-23 16:36:35 -0400

    MWPW-114916 Lazy load caas

--- libs/blocks/caas/caas.js
+++ libs/blocks/caas/caas.js
@@ -1,18 +1,41 @@
 import { initCaas, loadCaasFiles, loadStrings } from './utils.js';
 import { parseEncodedConfig } from '../../utils/utils.js';
 
-export default async function init(a) {
-  await loadCaasFiles();
+const rootMargin = 1000;
 
-  const encodedConfig = a.href.split('#')[1];
-  const state = parseEncodedConfig(encodedConfig);
-  const caasStrs = state.placeholderUrl ? await loadStrings(state.placeholderUrl) : {};
+const getCaasStrings = (placeholderUrl) => new Promise((resolve) => {
+  if (placeholderUrl) {
+    resolve(loadStrings(placeholderUrl));
+    return;
+  }
+  resolve({});
+});
 
-  const block = document.createElement('div');
-  block.className = a.className;
-  block.id = 'caas';
-  a.insertAdjacentElement('afterend', block);
-  a.remove();
+export default async function init(link) {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach(async (entry) => {
+      if (entry.isIntersecting) {
+        const a = entry.target;
+        observer.unobserve(a);
 
-  initCaas(state, caasStrs, block);
+        const encodedConfig = a.href.split('#')[1];
+        const state = parseEncodedConfig(encodedConfig);
+
+        const [caasStrs] = await Promise.all([
+          getCaasStrings(state.placeholderUrl),
+          loadCaasFiles(),
+        ]);
+
+        const block = document.createElement('div');
+        block.className = a.className;
+        block.id = 'caas';
+        a.insertAdjacentElement('afterend', block);
+        a.remove();
+
+        initCaas(state, caasStrs, block);
+      }
+    });
+  }, { rootMargin: `${rootMargin}px` });
+
+  io.observe(link);
 }
